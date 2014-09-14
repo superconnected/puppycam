276c8499a6daee8d26504ddc8ed15da10aa9b105
Josh Daniels <superconnected@gmail.com>
2014-09-13 22:25:27 -0500

    now reading authentication settings from config file

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, url_for
 from flask.ext.basicauth import BasicAuth
+import ConfigParser
+import os
 
 app = Flask(__name__)
 
-app.config['BASIC_AUTH_USERNAME'] = 'xxxx'
-app.config['BASIC_AUTH_PASSWORD'] = 'xxxx'
+config = ConfigParser.RawConfigParser()
+config_file = os.path.join(os.path.dirname(__file__), 'app.cfg')
+config.read(config_file)
+
+app.config['BASIC_AUTH_USERNAME'] = config.get('Auth', 'username')
+app.config['BASIC_AUTH_PASSWORD'] = config.get('Auth', 'password')
 app.config['BASIC_AUTH_FORCE'] = True
 
 basic_auth = BasicAuth(app)
