d434f4c051d4827038032451b8413f8d53fb8dd0
Josh Daniels <superconnected@gmail.com>
2014-10-24 10:58:48 -0500

    updated puppycam script to use the Raspberry Pi camera module instead of a regular webcam

--- puppycam.sh
+++ puppycam.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H%M%S")
+BASE=/home/pi/puppycam/website/static/img
 
-fswebcam --no-banner -S 50 /home/pi/puppycam/website/static/img/$DATE.jpg
+raspistill -vf -hf -w 640 -h 480 -o $BASE/full/$DATE.jpg
+convert $BASE/full/$DATE.jpg -resize 250x250 $BASE/thumbnails/$DATE.jpg
