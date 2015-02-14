8084606ff9bb7c591f618012aa01009bf5048043
Josh Daniels <superconnected@gmail.com>
2015-02-14 11:58:10 -0600

    removed vertical and horizontal flip args

--- puppycam.sh
+++ puppycam.sh
@@ -3,7 +3,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M%S")
 BASE=/home/pi/puppycam/website
 
-raspistill -vf -hf -w 640 -h 480 -o $BASE/img/full/$DATE.jpg
+raspistill -w 640 -h 480 -o $BASE/img/full/$DATE.jpg
 convert $BASE/img/full/$DATE.jpg -resize 250x250 $BASE/img/thumbs/$DATE.jpg
 
 python $BASE/generate_site.py
